a7b1902eca15cced069b999f17ad348c6d5a1f16
squall82 <squall.list@gmail.com>
2026-06-26 08:05:38 +0700

    Delete gemoney directory

--- gemoney/__init__.py
+++
@@ -1,3 +0,0 @@
-"""GE Money - aplikasi pencatatan keuangan pribadi (modular)."""
-
-__version__ = "1.0.0"
--- gemoney/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Komponen antarmuka (Tkinter) untuk tiap module."""
-
-from .app import GeMoneyApp
-
-__all__ = ["GeMoneyApp"]
